9ca6bb5f90711f93ab6746f145c3ed1b3ddf3ccc
Bianca Xavier <202794770+Biancaxs@users.noreply.github.com>
2026-07-24 12:36:34 -0300

    create book constructor

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
-  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
