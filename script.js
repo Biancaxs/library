8bbe74951b86c2e67c84a1187955e01c99e08698
Bianca Xavier <202794770+Biancaxs@users.noreply.github.com>
2026-07-24 13:47:29 -0300

    add initial forEach loop for displaying books

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const booksContainer = document.querySelector(".books-container")
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -12,3 +14,14 @@ function addBookToLibrary(title, author, pages, read) {
     book.id = crypto.randomUUID()
     myLibrary.push(book)
 }
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "not read yet")
+addBookToLibrary("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 309, "not read yet")
+
+myLibrary.forEach((book) => {
+    const books = document.createElement("div");
+    books.classList.add("books");
+    books.textContent = JSON.stringify(`${book.title} by ${book.author}, ${book.pages} pages, ${book.read}`)
+
+    booksContainer.appendChild(books)
+})
