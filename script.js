b0b84d8ca93494353f2813a744ad100ebfd4bbb7
Bianca Xavier <202794770+Biancaxs@users.noreply.github.com>
2026-07-24 12:34:24 -0300

    Add initial skeleton for book constructor and array handling

---
+++ script.js
@@ -0,0 +1,9 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
