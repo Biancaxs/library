efb18199748eef4fab3f90c38ad4ecee87f8b29d
Bianca Xavier <202794770+Biancaxs@users.noreply.github.com>
2026-07-24 15:15:16 -0300

    Fix DOM selection for dialog buttons

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const booksContainer = document.querySelector(".books-container")
 
+const dialog = document.querySelector("dialog")
+const form = document.querySelector("form") 
+const newBookBtn = document.querySelector(".new-book")
+const addBookBtn = document.querySelector(".add-book")
+const closeBtn = document.querySelector(".close-btn")
+
+const bookTitle = document.querySelector("#title")
+const bookAuthor = document.querySelector("#author")
+const bookPages = document.querySelector("#pages")
+
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -15,13 +26,21 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(book)
 }
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "not read yet")
-addBookToLibrary("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 309, "not read yet")
+//addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "not read yet")
+//addBookToLibrary("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 309, "not read yet")
 
 myLibrary.forEach((book) => {
     const books = document.createElement("div");
     books.classList.add("books");
-    books.textContent = JSON.stringify(`${book.title} by ${book.author}, ${book.pages} pages, ${book.read}`)
+    books.textContent = `${book.title} by ${book.author}, ${book.pages} pages, ${book.read}`
 
     booksContainer.appendChild(books)
+})
+
+form.addEventListener("submit", (e) => {
+     e.preventDefault()
+
+    const newTitle = bookTitle.value
+    const newAuthor = bookAuthor.value
+    const newPages = bookPages.value
 })
